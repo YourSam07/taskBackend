413c7e09323a0369950377e722f0e5a595096d49
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-07-29 22:23:04 +0530

    finaaly

--- userroutes.js
+++ userroutes.js
@@ -2,7 +2,7 @@ const express= require('express')
 const { registerUser, loginrUser, getUserData }= require('./Controller/userController')
 const router = express.Router()
 
-router.post('/', registerUser)
+router.post('/signup', registerUser)
 router.post('/login', loginrUser)
 
 module.exports = router
