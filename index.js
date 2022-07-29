85ad6961b2f2d24b90d70fce53b84fb629915fd5
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-07-29 22:35:59 +0530

    asdf

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.use(cors())
 
 app.use('/api/user', require('./userroutes'))
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("frontend", "build", "index.html"));
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running at port ${port}`))
