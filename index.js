4179c5b6c5781b81ad928f537d49974df71540ae
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-07-29 22:43:04 +0530

    ADAS

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ app.use(cors())
 
 app.use('/api/user', require('./userroutes'))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.send({
+    message: "good"
+  })
+};
 
 app.use(errorHandler);
 
